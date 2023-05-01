9eeb7596220edc539284e5243e2b07ef2c76b43a
jinjinew <jinwoo8365@gmail.com>
2023-05-01 11:30:44 +0900

    Feat. Add items on lists (Tralling)

---
+++ main.py
@@ -0,0 +1,20 @@
+# Test python env
+
+def print_hello():
+    animals = ['dog','cat','hamster', 'tiger'] # in one line
+    foods = [
+        'Spagetti',
+        'Pizza',
+	'bibimbob'
+    ] # w/o trailing comma
+    names = [
+        'John',
+        'Jane',
+        'Gil-dong',
+	'Dong-eun',
+    ] # w/ trailing comma
+    for f_name in names:
+        print(f'hello, {f_name}')
+
+if __name__ == '__main__':
+    print_hello()
